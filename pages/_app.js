2495908c84f63c51f83399e313fa1f2589570669
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2023-02-01 16:55:43 +0700

    initial project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import {ChakraProvider} from '@chakra-ui/react';
+import Layout from '@/components/layout/layout';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}) {
+  return (
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
 }
