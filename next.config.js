db0eea454bb276fdb5d3b6053b5de8e5b0f67224
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2023-02-06 22:57:41 +0700

    added next bundler

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
+});
+
+module.exports = {nextConfig, withBundleAnalyzer};
